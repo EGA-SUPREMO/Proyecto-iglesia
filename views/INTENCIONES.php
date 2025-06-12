5ccb1e73557be1acb093b7e6eacc872dbcc1b277
EGA-SUPREMO <cxiesestro@gmail.com>
2025-06-12 12:19:38 -0400

    add form para las plantillas restanttes y borrar las que no usaran

---
+++ views/INTENCIONES.php
@@ -0,0 +1,14 @@
+<fieldset>
+    <p>NOTA:En esta tengo que pedir que poner en cada texto</p>
+    <label for="titulo">Título del Documento:</label>
+    <input type="text" id="titulo" name="titulo" required>
+    <br>
+    <label for="nombre">Nombre Completo:</label>
+    <input type="text" id="nombre" name="nombre" required>
+    <br>
+    <label for="fecha">Fecha:</label>
+    <input type="date" id="fecha" name="fecha" required>
+    <br>
+    <label for="descripcion">Descripción:</label>
+    <textarea id="descripcion" name="descripcion" rows="5" required></textarea>
+</fieldset>
---
+++ views/fe de bautizo.php
@@ -0,0 +1,28 @@
+<fieldset>
+    <label for="nombre_bautizado">Nombre completo del Bautizado:</label>
+    <input type="text" id="nombre_bautizado" name="nombre_bautizado" required><br>
+
+    <br>
+
+    <label for="padre_1">Hijo/a de (Padre/Madre 1):</label>
+    <input type="text" id="padre_1" name="padre_1" required><br>
+
+    <label for="padre_2">Y (Padre/Madre 2):</label>
+    <input type="text" id="padre_2" name="padre_2" required><br>
+
+    <br>
+
+    <label for="fecha_bautismo">Fecha de Bautismo (DD de MES de AAAA):</label>
+    <input type="text" id="fecha_bautismo" name="fecha_bautismo" placeholder="Ej: 15 de Octubre de 2024" required><br>
+
+    <label for="padrino_1">Padrino/Madrina 1:</label>
+    <input type="text" id="padrino_1" name="padrino_1" required><br>
+
+    <label for="padrino_2">Y Padrino/Madrina 2:</label>
+    <input type="text" id="padrino_2" name="padrino_2" required><br>
+
+    <br>
+
+    <label for="nota_marginal">Nota Marginal (texto completo de la nota):</label>
+    <textarea id="nota_marginal" name="nota_marginal" rows="4"></textarea><br>
+</fieldset>
