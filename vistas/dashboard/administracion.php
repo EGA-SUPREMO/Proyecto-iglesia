0fab14ba5fc6666110f3f88d21bcb5f768873c74
EGA-SUPREMO <cxiesestro@gmail.com>
2025-12-30 12:44:07 -0400

    agregra link para editar misas incompleto

--- vistas/dashboard/administracion.php
+++ vistas/dashboard/administracion.php
@@ -35,6 +35,13 @@
                                 </div>
                             </a>
                         </div>
+                        <div class="col-sm-6">
+                            <a href="?c=panel&a=index&t=misa" class="card text-decoration-none text-dark h-100 lift-effect">
+                                <div class="card-body text-center">
+                                    <h5 class="card-title mb-0">Gestionar Misas</h5>
+                                </div>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </section>
