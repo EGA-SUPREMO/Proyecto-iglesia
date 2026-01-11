5bdc2b4e8932894a005fc486dbb4b421a2746c0a
EGA-SUPREMO <cxiesestro@gmail.com>
2026-01-11 11:09:30 -0400

    borrar metodo sin usar en servicio misa.php

--- modelo/ServicioMisa.php
+++ modelo/ServicioMisa.php
@@ -13,42 +13,19 @@ class ServicioMisa extends ServicioBase
         $this ->gestorMisa = new GestorMisa($pdo);
     }
 
-    /*
+    /* METODO EN DESUSO
+
     Comparación: El API compara:
-
     Fecha pedida (hasta): 05/01/2026.
-
     Fecha máxima en BD: 31/12/2025.
-
     Conclusión: ¡Faltan misas! Faltan las de los días 1, 2, 3, 4 y 5 de Enero.
-
     Generación (JIT): El API antes de continuar, llama a una función interna: GenerarMisasHasta('2026-01-05').
-
     Esta función usa tu plantilla de horario (L-S 7pm, DOM 8am, DOM 12pm, DOM 7pm).
-
     Calcula los días que faltan (01/01/2026 al 05/01/2026).
-
     Crea los registros en la tabla misas solo para esos 5 días (probablemente 1 misa el jueves, 1 el viernes, 1 el sábado, 3 el domingo, 1 el lunes).
-
     Importante: Esto debe hacerse en una transacción para evitar que dos secretarias lo hagan al mismo tiempo.
-
     Consulta Final: Una vez generadas las misas que faltaban, el API ahora sí ejecuta la consulta final (la que iba a hacer en el paso 3) y devuelve a la UI la lista completa de misas disponibles entre el 28/12 y el 05/01.
-
     */
-    public function guardar($hastaFecha, $id = 0)
-    {
-        $this->pdo->beginTransaction();
-        try {
-
-            $this->pdo->commit();
-            return $resultado;
-        } catch (Exception $e) {
-            $this->pdo->rollBack();
-            error_log("Error en la transacción de : " . $e->getMessage());
-            throw new Exception($e->getMessage());
-        }
-    }
-
     public function generarMisasEnRango($inicioFecha, $hastaFecha)
     {
         // TODO hacer comprobaciones de que fecha de inicio ya esta hecha, ej pido crear una misa el dia de hoy hasta tres dias despues, pero hay misas en la base de datos hasta mañana, solo se crea apartir de mañana
